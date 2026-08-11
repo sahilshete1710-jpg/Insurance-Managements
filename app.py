106654e7ef6678e3799085db9e3e2f0ceab95be5
Sahil Shete <sahilshete1710@gmail.com>
2026-08-11 11:07:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -17,7 +17,14 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
+def load_css():
+    with open("assets/style.css", "r", encoding="utf-8") as f:
+        st.markdown(
+            f"<style>{f.read()}</style>",
+            unsafe_allow_html=True
+        )
 
+load_css()
 
 # =========================================================
 # INITIALIZE DATABASE
